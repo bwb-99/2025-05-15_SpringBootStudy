1efa7a471afc5991949ffc2cd1220b294e1fd1e8
bae <bottleright10@gmail.com>
2025-05-30 14:22:54 +0900

    2025-05-25

--- SpringBootReduxLastProject/src/main/java/com/sist/web/dao/BusanFoodRepository.java
+++ SpringBootReduxLastProject/src/main/java/com/sist/web/dao/BusanFoodRepository.java
@@ -9,17 +9,30 @@ import com.sist.web.entity.BusanFoodEntity;
 import com.sist.web.vo.BusanFoodVO;
 
 import java.util.*;
-
+/*
+ * public int getNo();
+   public String getPoster();
+   public String getName();
+   public int getHit();
+   public int getJjimcount();
+   public int getLikecount();
+   public double getScore();
+   public String getType();
+ */
 @Repository
-public interface BusanFoodRepository extends JpaRepository<BusanFoodEntity, Integer> {
-   @Query(value = "SELECT fno, poster, name, hit, jjimcount, likecount, score, type, theme, content "
-         + "FROM busan_food ORDER BY fno ASC LIMIT 0, 9", nativeQuery = true)
-   public List<BusanFoodVO> busanMainData();
-   
-   @Query(value="SELECT cno, poster, name, hit, jjimcount, likecount, score, type, theme, content "
-         + "FROM busan_food ORDER BY cno ASC LIMIT :start, 12", nativeQuery = true)
-   public List<BusanFoodVO> busanListData(@Param("start") int start);
-   
-   @Query(value="SELECT CEIL COUNT(*)/12.0 FROM busan_food")
-   public int busanFoodTotalPage();
+public interface BusanFoodRepository extends JpaRepository<BusanFoodEntity, Integer>{
+    @Query(value="SELECT fno,poster,name,hit,jjimcount,likecount,score,type,theme,content "
+    	  +"FROM busan_food ORDER BY fno ASC "
+    	  +"LIMIT 0,9",nativeQuery = true)
+    public List<BusanFoodVO> busanMainData();
+    
+    @Query(value="SELECT fno,poster,name,hit,jjimcount,likecount,score,type,theme,content "
+      	  +"FROM busan_food ORDER BY fno ASC "
+      	  +"LIMIT start,12",nativeQuery = true)
+    public List<BusanFoodVO> busanListData(@Param("start") int start);
+    
+    @Query(value="SELECT CEIL(COUNT(*)/12.0) FROM busan_food")
+    public int busanFoodTotalPage();
+    // SELECT * FROM busan_food WHERE fno=?
+    public BusanFoodEntity findByFno(@Param("fno") int fno);
 }
