cb5f0151d1d87ecb8a240c8ad7a353775e099902
bae <bottleright10@gmail.com>
2025-06-02 16:38:59 +0900

    2025-06-02

--- SpringBootReactQuery_1/src/main/java/com/sist/web/dao/RecipeRepository.java
+++ SpringBootReactQuery_1/src/main/java/com/sist/web/dao/RecipeRepository.java
@@ -8,21 +8,18 @@ import java.util.*;
 import com.sist.web.entity.*;
 @Repository
 public interface RecipeRepository extends JpaRepository<RecipeEntity, Integer>{
-	@Query(value = "SELECT no, poster, chef, hit, likecount, num, title FROM ( " +
-	        "SELECT r.no, r.poster, r.chef, r.hit, r.likecount, r.title, " +
-	        "ROW_NUMBER() OVER (ORDER BY r.no ASC) AS num " +
-	        "FROM recipe r " +
-	        "WHERE r.no IN (SELECT DISTINCT rd.no FROM recipeDetail rd) " +
-	        ") AS sub " +
-	        "WHERE num BETWEEN :start AND :end", nativeQuery = true)
-	public List<RecipeVO> recipeListData(@Param("start") Integer start,
-	                                     @Param("end") Integer end);
-
+   @Query(value="SELECT no,poster,chef,hit,likecount,num,title "
+		 +"FROM (SELECT no,poster,chef,hit,likecount,rownum as num,title "
+		 +"FROM (SELECT /* INDEX_ASC(recipe recipe_no_pk)*/no,poster,chef,hit,likecount,title "
+		 +"FROM recipe WHERE no IN(SELECT no FROM recipe INTERSECT SELECT no FROM recipeDetail))) "
+		 +"WHERE num BETWEEN :start AND :end",nativeQuery = true)
+   public List<RecipeVO> recipeListData(@Param("start") Integer start,
+		       @Param("end") Integer end);
    
    public RecipeEntity findByNo(int no);
    
    @Query(value="SELECT CEIL(COUNT(*)/12.0) FROM recipe "
-		 +"WHERE no IN (SELECT DISTINCT no FROM recipeDetail)",nativeQuery = true)
+		 +"WHERE no IN(SELECT no FROM recipe INTERSECT SELECT no FROM recipeDetail)",nativeQuery = true)
    public int recipeTotalPage();
    // Find 
    // Chef 
